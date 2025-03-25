3e093a946922d182af86f00a2b4054bbbc853bc4
Lucas Emanuel Alves De Andrade <emanuellucasandrade@gmail.com>
2025-03-24 22:21:54 -0300

    Initial commit

---
+++ src/Transactions/Transacoes.java
@@ -0,0 +1,77 @@
+package Transactions;
+import Users.Pessoa;
+
+public abstract class Transacoes {
+	protected String data;
+	protected double valor;
+	protected String tipo;
+	protected String categoria;	
+	protected String descricao;
+	protected Pessoa usuario;
+
+	public Transacoes(String data, double valor, String tipo, String categoria, String descricao) {
+		this.data = data;
+		this.valor = valor;
+		this.tipo = tipo;
+		this.categoria = categoria;
+		this.descricao = descricao;
+	}
+
+	public void exibirDetalhes() {
+		System.out.println("");
+		System.out.println("-----------");
+		System.out.println(tipo + ":");
+		System.out.println("Data:" + data);
+		System.out.println("Valor:"+ valor);
+		System.out.println("Categoria:" + categoria);
+		System.out.println("Transação:" + descricao);
+		
+		if (usuario != null) {
+			System.out.println("Usuário: " + usuario.getNome());
+		} else {
+			System.out.println("Essa transação ainda não possui usuário!");
+		}
+	}
+	
+	public void setUsuario(Pessoa usuario) {
+		this.usuario = usuario;
+	}
+	public Pessoa getUsuario() {
+		return this.usuario;
+	}
+	
+	public String getData() {
+		return this.data;	
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Double getValor() {
+		return this.valor;	
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCategoria() {
+		return this.categoria;	
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return this.descricao;	
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
